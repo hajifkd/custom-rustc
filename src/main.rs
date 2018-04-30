2cda9e472047c66cc8ac8d5e4977790026870f9b
hfukuda <hajime.fukuda@me.com>
2018-04-30 23:42:57 +0900

    print some info

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ extern crate getopts;
 extern crate rustc;
 extern crate rustc_driver;
 
+use rustc::session::config::Input;
 use rustc::session::Session;
 use rustc_driver::driver::{CompileController, CompileState};
 use rustc_driver::{run_compiler, Compilation, CompilerCalls};
+
 use std::env;
 
 struct MyRustcCall;
@@ -19,8 +21,20 @@ impl<'a> CompilerCalls<'a> for MyRustcCall {
     ) -> CompileController<'a> {
         let mut controller = CompileController::basic();
         controller.after_parse.stop = Compilation::Stop;
-        controller.after_parse.callback = Box::new(|_state: &mut CompileState| {
+        controller.after_parse.callback = Box::new(|state: &mut CompileState| {
             println!("Hello, my compiler!",);
+
+            // take a look at `input`
+            match state.input {
+                Input::File(ref p) => {
+                    println!("{:?}", p);
+                }
+                Input::Str { name, input } => {
+                    println!("name: {}, input: {}", name, input);
+                }
+            }
+
+            println!("{:?}", state.krate);
         });
 
         controller
